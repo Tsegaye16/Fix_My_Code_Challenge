d1380a86f84b3d3708964446521f8df2cd66cec9
Tsegaye16 <abewatsegaye16@gmail.com>
2023-08-18 05:09:51 -0700

    update

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 /*
     Print a square with the character #
-
-    The size of the square must be the first argument
+    
+    The size of the square must be the first argument 
     of the program.
 */
 
@@ -14,10 +14,10 @@ if (process.argv.length <= 2) {
     process.exit(1)
 }
 
-size = parseInt(process.argv[2], 10)
+size = parseInt(process.argv[2], 16)
 
-for (let i = 0 ; i < size ; i++) {
-    for (let j = 0 ; j < size ; j++) {
+for (let i = 0 ; i < size ; i ++) {
+    for (let j = 0 ; j < size ; j ++) {
         process.stdout.write("#");
     }
     process.stdout.write("\n");
